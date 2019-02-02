bc5c95021875a9878ebbdf9e3c92ced7cb9729a8
Sinclair Target <sinclairtarget@gmail.com>
2019-02-02 15:50:42 -0600

    Business formation plot now annual instead of just Q4.

--- plots/business-formation.r
+++ plots/business-formation.r
@@ -4,32 +4,37 @@ library('scales')
 df <- read_csv('business-formation-data/BF4Q_ST.csv')
 df <- df %>%
       gather(state, n, -Time) %>%
-      filter(grepl('*Q4$', Time)) %>%
-      mutate(Time = as.factor(gsub(' Q[0-9]$', '', Time))) %>%
-      rename(time = Time) %>%
-      mutate(state = as.factor(state))
+      mutate(year = as.integer(gsub(' Q[0-9]$', '', Time)),
+             quarter = as.factor(gsub('^[0-9]* Q', '', Time)),
+             state = as.factor(state)) %>%
+      select(-Time) %>%
+      # Only have data for all quarters between these years
+      filter(year > 2004 & year <= 2015) %>%
+      mutate(year = as.factor(year))
+
+# Group quarters so we get annual count
+df <- df %>% group_by(state, year) %>% summarize(n = sum(n))
 
 # We want to highlight CA, NY, WA, TX
 df <- df %>%
       mutate(highlight = as.factor(ifelse(state %in% c('NY', 'CA', 'WA', 'TX'),
                                     state,
                                     NA)))
-df
 
-to_label <- df %>%
-            filter(!is.na(highlight) & time == 2015)
+ to_label <- df %>%
+             filter(!is.na(highlight) & year == 2015)
 
 # ---- plot.formation ----
-ggplot(df, aes(x = time, y = n, color = highlight)) +
+ggplot(df, aes(x = year, y = n, color = highlight)) +
     geom_line(data=filter(df, is.na(highlight)), aes(group = state)) +
     geom_line(data=filter(df, !is.na(highlight)), aes(group = state), size = 1) +
     geom_label(data=to_label, aes(label = state)) +
-    scale_y_continuous(limits = c(0, 15000),
-                       breaks=seq(0, 15000, 2500),
+    scale_y_continuous(limits = c(0, 70000),
+                       breaks = seq(0, 70000, 10000),
                        labels = comma) +
     guides(color = FALSE) +
-    labs(title = 'Americans Are Starting Fewer Businesses, Even in California',
-         subtitle = 'Q4 Business Formations by Year',
+    labs(title = 'Americans Are Starting Fewer Businesses Than a Decade Ago',
+         subtitle = 'Business Formations by Year',
          captions = 'Source: US Census Bureau Business Formation Statistics',
          x = 'Year',
          y = 'Number of Businesses')
